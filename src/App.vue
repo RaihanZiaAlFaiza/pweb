bbe560d018714ed4510b6b877a9106a2705f326a
Raihanzia <raihanziaalfaiza@gmail.com>
2023-04-05 11:36:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+
+<script setup>
+
+</script>
+
+
+
+  
+
+<template>
+    <h1><center>COFEE SHOP</center></h1>
+    <center><img src="https://o.remove.bg/downloads/60f1d1ed-3c84-40d5-9b14-157e1ce37884/cofee-removebg-preview.png" alt="Logo Games" style="width:500px;height:230px;"/></center>
+ <h2><center>Login Coffee Shop</center></h2>
+ <form action="/action_page.php"><center>
+        <label for="usname">Username:</label>
+        <input type="text" id="usname" name="usname"><br><br>
+        <label for="password">Password:</label>
+        <input type="text" id="Password" name="Password"><br><br>
+        <input type="submit" value="Submit"></center>
+        <h3>Menu : </h3> 
+    <table style="width:20% ;">
+        <tr>
+            <th>Menu</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>Red Velvet</td>
+            <td>20k</td>
+        </tr>
+        <tr>
+            <td>coffe latte</td>
+            <td>15k</td>
+        </tr>
+        <tr>
+            <td>Matcha</td>
+            <td>20k</td>
+        </tr>
+        <tr>
+            <td>Black coffe</td>
+            <td>22k</td>
+        </tr>
+        <tr>
+            <td>Cappucino</td>
+            <td>20k</td>
+        </tr>
+        <tr>
+            <td>Taro</td>
+            <td>18k</td>
+        </tr>
+        <tr>
+            <td>Green tea</td>
+            <td>20k</td>
+        </tr>
+        <tr>
+            <td>Americano</td>
+            <td>15k</td>
+        </tr>
+        <tr>
+            <td>Coffe Signature</td>
+            <td>20k</td>
+        </tr>
+        <tr>
+            <td>Air Kosong</td>
+            <td>20k</td>
+        </tr>
+    </table>
+    <form action="/action_page.php" ><label for="drink" >Choose a drinks: </label>
+  <select name="drink" id="drink">
+    <option value="Red velvet">Red velvet</option>
+    <option value="coffe latte">coffe latte</option>
+    <option value="matcha">matcha</option>
+    <option value="black coffe">black coffe</option>
+    <option value="Cappucino">Cappucino</option>
+    <option value="Taro">Taro</option>
+    <option value="Green tea">Green tea</option>
+    <option value="Americano">Americano</option>
+    <option value="Coffe Signature">Coffe Siganture</option>
+    <option value="Air Kosong">air Kosong</option>
+    
+  </select>
+  <br><br>
+  <input type="submit" value="Submit"></form>
+</form>
+    <form action="/action_page.php">
+        <p><label for="Cofee Shop">Jika ada saran atau masukan comment dibawah ini:</label></p>
+        <textarea id="Cofee Shop" name="Coffe Shop" rows="4" cols="50">Kritik Dan Saran</textarea>
+        <br>
+    </form>
+    <p>Dapatkah kamu menunjukkan saya dimana lokasinya?: Klik
+            <a href="https://maps.app.goo.gl/bmK7MPKaWPQKKLpSA">DISINI! </a>
+        </p>
+   
+<h4><center>Terima Kasih</center></h4>
+<h5><center>Sampai Berjumpa Lagi...</center></h5>
+<h6><center>create by : Raihan Zia Al-faiza</center></h6>
+</template>
+
+<style scoped>
+  
+
+</style>
+<style>
+h6 {margin-left: 85%; }
+h5 {margin-top: -1%;} 
+form{margin-top: 5%;}
+h3 {color: rgb(1, 8, 15);}
+body {
+    background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShmn2t_WIrqK-6K7dtqDTegfjTvYgW5GoLSTHg_oqgX1TAZQIHfwjDQ2KO_JhzuOnIOWw&usqp=CAU");
+  background-size: cover;}
+  table, th, td {border:2px solid #433f3f; color: rgb(1, 8, 15);} 
+  form,p {color: rgb(1, 8, 15);}
+  h2 {color: rgb(1, 8, 15);}
+</style>
+
